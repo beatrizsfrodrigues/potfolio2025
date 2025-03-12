cb03ad596c479f26f79b50cf45a38a1e879e0431
Beatriz <40210313@esmad.ipp.pt>
2025-03-12 11:13:20 +0000

    Refactor project card layout and styling; update dimensions, improve responsiveness, and enhance project information display

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,49 +37,54 @@ export default function Projects() {
       <h1 className="pageTitle firstDiv">Projects</h1>
       <Flex gap="4" height="80vh">
         <div className="lineContainer" id="projectInfo">
-          projeto
+          <h2>Titulo</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            volutpat tincidunt mi tristique convallis. Vestibulum viverra
+            pulvinar mauris, eu molestie velit rutrum non.
+          </p>
         </div>
-        <div className="lineContainer" id="projectsContainer">
-          <Flex wrap="wrap" gap="4" id="coisa">
-            {[
-              "Training Wheels",
-              "Project 2",
-              "Project 3",
-              "Evergreen",
-              "Eco-Meow",
-            ].map((title, index) => (
-              <Card
-                key={index}
-                className="projectCard"
-                onMouseEnter={() => handleMouseEnter(index)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <h2 className="titleCard">{title}</h2>
-                {hoveredCard === index ? (
-                  <div>
-                    <Flex gap="1" wrap="wrap">
-                      <Badge>ola</Badge>
-                    </Flex>
+        <div className="lineContainer" id="projectsPContainer">
+          {/* <div id="coisa"> */}
+          {[
+            "Training Wheels",
+            "Project 2",
+            "Project 3",
+            "Evergreen",
+            "Eco-Meow",
+          ].map((title, index) => (
+            <Card
+              key={index}
+              className="projectCard cardsProjects"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <h2 className="titleCard">{title}</h2>
+              {hoveredCard === index ? (
+                <div>
+                  <Flex gap="1" wrap="wrap">
+                    <Badge>ola</Badge>
+                  </Flex>
 
-                    <Button radius="full" variant="solid" size="3">
-                      Learn more
-                    </Button>
-                  </div>
-                ) : (
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec volutpat tincidunt mi tristique convallis. Vestibulum
-                    viverra pulvinar mauris, eu molestie velit rutrum non.
-                  </p>
-                )}
-                <img
-                  src="https://static.vecteezy.com/system/resources/thumbnails/012/986/755/small/abstract-circle-logo-icon-free-png.png"
-                  alt=""
-                  className="projectCardImg"
-                />
-              </Card>
-            ))}
-          </Flex>
+                  <Button radius="full" variant="solid" size="3">
+                    Learn more
+                  </Button>
+                </div>
+              ) : (
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  volutpat tincidunt mi tristique convallis. Vestibulum viverra
+                  pulvinar mauris, eu molestie velit rutrum non.
+                </p>
+              )}
+              <img
+                src="https://static.vecteezy.com/system/resources/thumbnails/012/986/755/small/abstract-circle-logo-icon-free-png.png"
+                alt=""
+                className="projectCardImg"
+              />
+            </Card>
+          ))}
+          {/* </div> */}
         </div>
       </Flex>
     </div>
